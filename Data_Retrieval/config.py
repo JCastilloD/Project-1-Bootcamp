5eaadf6732cf934c3b38e79efd20ebc05927e57d
Joshua Castillo <56041366+JCastilloD@users.noreply.github.com>
2020-02-06 21:13:53 -0600

    Delete config.py
    Use your own keys!

--- Data_Retrieval/config.py
+++
@@ -1,6 +0,0 @@
-# Joshua's Google Key
-gkey = "AIzaSyAwDyEwcf_ffX0otnRMJYklewZ9ywIkxJc"
-# Monica's Coneval Key
-coneval_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vbmljYW1jOTJAZ21haWwuY29tIiwicm9sZSI6IkV4dGVybm9zIiwibmJmIjoxNTc5OTgxNTkwLCJleHAiOjE1ODAwNjc5OTAsImlhdCI6MTU3OTk4MTU5MCwiaXNzIjoic2VsZiIsImF1ZCI6ImxvY2FsaG9zdDo4NCJ9.Ft7b3X_jg6UrTIlPKNlm5ZTXMc3BIXIEcorZhp3m3ec"
-# Inegi_Key
-inegi_key = "8455939b-9726-bb8c-c70d-681609b40110"
